f66053c25f193db7f71decbe3f0a3a01f772ef01
Ladanov Nikolay <ladanov_n@mail.ru>
2020-02-09 07:45:27 +0300

    Updates

---
+++ precache-manifest.842ee9f46d1897aaf0127bb582bc0d0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0b9b2ecccdddfc5b38450758f8211f",
+    "url": "/moscowTravel/frontend/index.html"
+  },
+  {
+    "revision": "60e243985236d8448d3d",
+    "url": "/moscowTravel/frontend/static/css/main.1148e9f6.chunk.css"
+  },
+  {
+    "revision": "cc2cbb4fe3bc6e5527ac",
+    "url": "/moscowTravel/frontend/static/js/2.b3f01657.chunk.js"
+  },
+  {
+    "revision": "60e243985236d8448d3d",
+    "url": "/moscowTravel/frontend/static/js/main.040dfb18.chunk.js"
+  },
+  {
+    "revision": "310aa5af537da8fcf04e",
+    "url": "/moscowTravel/frontend/static/js/runtime-main.878f73ad.js"
+  }
+]);
