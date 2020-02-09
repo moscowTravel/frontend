ceaa99b745f891d5a408a0335a2bee2603def2be
Ladanov Nikolay <ladanov_n@mail.ru>
2020-02-09 08:14:08 +0300

    Updates

---
+++ precache-manifest.d916b6917c20370afacdcc9399f08314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0a085989b88404a03fdc91004078c6",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "c03f407760a6ca9b0c9c",
+    "url": "/frontend/static/css/main.1148e9f6.chunk.css"
+  },
+  {
+    "revision": "cc2cbb4fe3bc6e5527ac",
+    "url": "/frontend/static/js/2.b3f01657.chunk.js"
+  },
+  {
+    "revision": "c03f407760a6ca9b0c9c",
+    "url": "/frontend/static/js/main.11224f20.chunk.js"
+  },
+  {
+    "revision": "fb52525d4ed97278efc8",
+    "url": "/frontend/static/js/runtime-main.348b76d0.js"
+  }
+]);
